dced08fcd831c4ff124debfdcfd08639fd45e6ab
David Eadie <david.eadie@red-gate.com>
2015-05-14 16:59:17 +0100

    Test for multiple games

--- UnitTests/LeagueTests.cs
+++ UnitTests/LeagueTests.cs
@@ -1,5 +1,4 @@
 ﻿using System.Linq;
-using System.Linq.Expressions;
 using Xunit;
 
 namespace ELORank.UnitTests
@@ -30,6 +29,28 @@ namespace ELORank.UnitTests
 
             Assert.Equal(5.0, league.GetLeaderBoard().Where(x => x.Name == "Foo").Select(x => x.Score).Single());
             Assert.Equal(1.0, league.GetLeaderBoard().Where(x => x.Name == "Bar").Select(x => x.Score).Single());
+        }
+
+        [Fact]
+        public void CanRecordMultipleSimpleGame()
+        {
+            var league = new League(new SimpleScoringStrategy());
+            league.AddPlayer("Foo");
+            league.AddPlayer("Bar");
+
+            var game = new Game();
+            game.AddResult("Foo", 5);
+            game.AddResult("Bar", 1);
+
+            var game2 = new Game();
+            game2.AddResult("Foo", 3);
+            game2.AddResult("Bar", 2);
+
+            league.RecordGame(game);
+            league.RecordGame(game2);
+
+            Assert.Equal(8.0, league.GetLeaderBoard().Where(x => x.Name == "Foo").Select(x => x.Score).Single());
+            Assert.Equal(3.0, league.GetLeaderBoard().Where(x => x.Name == "Bar").Select(x => x.Score).Single());
         }
     }
 }
