8b673a28222eb9cd7c1f4165b5ef52a35a040664
David Eadie <david.eadie@red-gate.com>
2015-05-15 16:24:47 +0100

    Red: ELO gives correct result after 1 round with 2 players

---
+++ UnitTests/EloTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace ELORank.UnitTests
+{
+    public class EloTests
+    {
+        [Fact]
+        public void TwoPlayerGameOneRound()
+        {
+            var league = new League(new EloScoringStrategy());
+
+            league.AddPlayer("Foo");
+            league.AddPlayer("Bar");
+
+            var game = new Game();
+
+            game.AddResult("Foo", 100);
+            game.AddResult("Bar", 0);
+
+            league.RecordGame(game);
+
+            Assert.Equal(1400 + 16, league.GetLeaderBoard().Where(x => x.Name == "Foo").Select(x => x.Score).Single());
+            Assert.Equal(1400 - 16, league.GetLeaderBoard().Where(x => x.Name == "Bar").Select(x => x.Score).Single());
+        }
+
+    }
+}
