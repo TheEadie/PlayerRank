82dccc8a8ff07e2c703e524bf2c3b41f44ef646d
David Eadie <david.eadie@red-gate.com>
2015-05-28 17:09:51 +0100

    Added test for multiple 2-player games.

--- UnitTests/EloTests.cs
+++ UnitTests/EloTests.cs
@@ -24,5 +24,35 @@ namespace ELORank.UnitTests
             Assert.Equal(1400 - 16, league.GetLeaderBoard().Where(x => x.Name == "Bar").Select(x => x.Score).Single());
         }
 
+        [Fact]
+        public void TwoPlayerGame20Rounds()
+        {
+            var league = new League(new EloScoringStrategy());
+
+            league.AddPlayer("Foo");
+            league.AddPlayer("Bar");
+
+            for (var i = 0; i < 10; i++)
+            {
+                var game = new Game();
+
+                game.AddResult("Foo", 100);
+                game.AddResult("Bar", 0);
+
+                league.RecordGame(game);
+
+                var game2 = new Game();
+
+                game2.AddResult("Foo", 0);
+                game2.AddResult("Bar", 100);
+
+                league.RecordGame(game2);
+            }
+            
+            // Foo won most recently therefore will be slightly ahead
+            Assert.Equal(1394, league.GetLeaderBoard().Where(x => x.Name == "Foo").Select(x => x.Score).Single());
+            Assert.Equal(1406, league.GetLeaderBoard().Where(x => x.Name == "Bar").Select(x => x.Score).Single());
+        }
+
     }
 }
