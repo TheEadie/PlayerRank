e55df1aec2ce11dadc170a85d51bc16482e7a793
David Eadie <david@eadie.net>
2015-07-26 20:43:41 +0100

    Red: Can discard worst result

--- PlayerRank/Scoring/LowestPointsStrategy.cs
+++ PlayerRank/Scoring/LowestPointsStrategy.cs
@@ -5,6 +5,13 @@ namespace PlayerRank.Scoring
 {
     internal class LowestPointsStrategy : IScoringStrategy
     {
+        private int m_discards;
+
+        public LowestPointsStrategy(int discards = 0)
+        {
+            m_discards = discards;
+        }
+
         public IList<PlayerScore> UpdateScores(IList<PlayerScore> scoreboard, Game game)
         {
             foreach (var result in game.GetResults())
--- UnitTests/LowestPointsTests.cs
+++ UnitTests/LowestPointsTests.cs
@@ -22,5 +22,40 @@ namespace PlayerRank.UnitTests
             Assert.Equal(1, league.GetLeaderBoard(scoringStrategy).Where(x => x.Name == "Foo").Select(x => x.Score).Single());
             Assert.Equal(2, league.GetLeaderBoard(scoringStrategy).Where(x => x.Name == "Bar").Select(x => x.Score).Single());
         }
+
+        [Fact]
+        public void TwoPlayerGameFourRoundsWithOneDiscard()
+        {
+            var league = new League();
+
+            var game = new Game();
+
+            game.AddResult("Foo", 1);
+            game.AddResult("Bar", 2);
+
+            var game2 = new Game();
+
+            game2.AddResult("Foo", 1);
+            game2.AddResult("Bar", 5);
+
+            var game3 = new Game();
+
+            game3.AddResult("Foo", 2);
+            game3.AddResult("Bar", 1);
+
+            var game4 = new Game();
+
+            game4.AddResult("Foo", 3);
+            game4.AddResult("Bar", 1);
+
+            league.RecordGame(game);
+            league.RecordGame(game2);
+            league.RecordGame(game3);
+            league.RecordGame(game4);
+
+            var scoringStrategy = new LowestPointsStrategy(1);
+            Assert.Equal(4, league.GetLeaderBoard(scoringStrategy).Where(x => x.Name == "Foo").Select(x => x.Score).Single());
+            Assert.Equal(4, league.GetLeaderBoard(scoringStrategy).Where(x => x.Name == "Bar").Select(x => x.Score).Single());
+        }
     }
 }
