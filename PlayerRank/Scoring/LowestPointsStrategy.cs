0a0e7521f98e71c6acd939f26ee343fdc577c2f0
David Eadie <david@eadie.net>
2015-07-26 21:42:44 +0100

    Green: Discard worst results

--- PlayerRank/Scoring/LowestPointsStrategy.cs
+++ PlayerRank/Scoring/LowestPointsStrategy.cs
@@ -20,6 +20,13 @@ namespace PlayerRank.Scoring
 
         public IList<PlayerScore> UpdateScores(IList<PlayerScore> scoreboard, Game game)
         {
+
+            var allResultsPrev = m_allResults.SelectMany(x => x.GetResults()).ToList();
+
+            m_allResults.Add(game);
+
+            var allResultsNow = m_allResults.SelectMany(x => x.GetResults());
+
             foreach (var result in game.GetResults())
             {
                 var player = scoreboard.SingleOrDefault(p => p.Name == result.Key);
@@ -32,9 +39,29 @@ namespace PlayerRank.Scoring
                 }
 
                 player.AddScore(result.Value);
+
+                // Add back previous worst results
+                FindWorstResults(allResultsPrev, player, false);
+
+                // Subtract worst results
+                FindWorstResults(allResultsNow, player, true);
             }
 
             return scoreboard;
+        }
+
+        private void FindWorstResults(IEnumerable<KeyValuePair<string, double>> allResultsNow, PlayerScore player, bool subtract)
+        {
+            var allResultsForPlayer = allResultsNow.Where(x => x.Key == player.Name).Select(x => x.Value).OrderByDescending(x => x).ToList();
+
+            for (int i = 0; i < m_discards; i++)
+            {
+                if (allResultsForPlayer.Count > i)
+                {
+                    var nextWorstScore = subtract ? -allResultsForPlayer[i] : allResultsForPlayer[i];
+                    player.AddScore(nextWorstScore);
+                }
+            }
         }
     }
 }
