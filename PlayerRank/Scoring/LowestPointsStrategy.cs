ec3031233f54628821c6ab36de73c64177c68cf6
David Eadie <david@eadie.net>
2015-07-26 20:35:58 +0100

    Red: Lowest Points Strategy adds up scores

---
+++ PlayerRank/Scoring/LowestPointsStrategy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PlayerRank.Scoring
+{
+    internal class LowestPointsStrategy : IScoringStrategy
+    {
+        public LowestPointsStrategy()
+        {
+        }
+
+        public IList<PlayerScore> UpdateScores(IList<PlayerScore> scoreboard, Game game)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnitTests/LowestPointsTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using PlayerRank.Scoring;
+using Xunit;
+
+namespace PlayerRank.UnitTests
+{
+    public class LowestPointsTests
+    {
+        [Fact]
+        public void TwoPlayerGameOneRound()
+        {
+            var league = new League();
+
+            var game = new Game();
+
+            game.AddResult("Foo", 1);
+            game.AddResult("Bar", 2);
+
+            league.RecordGame(game);
+
+            var scoringStrategy = new LowestPointsStrategy();
+            Assert.Equal(1, league.GetLeaderBoard(scoringStrategy).Where(x => x.Name == "Foo").Select(x => x.Score).Single());
+            Assert.Equal(2, league.GetLeaderBoard(scoringStrategy).Where(x => x.Name == "Bar").Select(x => x.Score).Single());
+        }
+    }
+}
