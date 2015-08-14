c3dd6a0667f067c13b41a5d93797c4cc0fec8726
David Eadie <david.eadie@red-gate.com>
2015-08-14 13:57:07 +0100

    Red: Can change players score with new obj

--- PlayerRank/PlayerScore.cs
+++ PlayerRank/PlayerScore.cs
@@ -1,13 +1,21 @@
-﻿namespace PlayerRank
+﻿using System;
+
+namespace PlayerRank
 {
     public class PlayerScore
     {
         public string Name { get; set; }
         public double Score { get; internal set; }
+        public Score Rating { get; private set; }
 
         public PlayerScore(string name)
         {
             Name = name;
+        }
+
+        internal void AddScore(Score score)
+        {
+            
         }
 
         internal void AddScore(double score)
--- UnitTests/PlayerTests.cs
+++ UnitTests/PlayerTests.cs
@@ -8,14 +8,18 @@ namespace PlayerRank.UnitTests
         public void CanIncreasePlayersScore()
         {
             var player = new PlayerScore("Foo");
-            player.AddScore(100);
+            player.AddScore(new Score(100));
+
+            Assert.Equal(new Score(100), player.Rating);
         }
 
         [Fact]
         public void CanReducePlayersScore()
         {
             var player = new PlayerScore("Foo");
-            player.AddScore(-100);
+            player.AddScore(new Score(-100));
+
+            Assert.Equal(new Score(-100), player.Rating);
         }
     }
 }
