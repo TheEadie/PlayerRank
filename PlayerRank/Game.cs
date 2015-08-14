a1e7f4a54e58c9ead4204ef420d4f50d5ffbce72
David Eadie <david.eadie@red-gate.com>
2015-08-14 14:42:14 +0100

    Green: Can add results to game with new obj

--- PlayerRank/Game.cs
+++ PlayerRank/Game.cs
@@ -1,26 +1,45 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PlayerRank
 {
     public class Game
     {
-        private readonly Dictionary<string, double> m_Scores = new Dictionary<string, double>();
+        private readonly Dictionary<string, Points> m_Scores = new Dictionary<string, Points>();
 
         public void AddResult(string name, double score)
         {
             if (m_Scores.ContainsKey(name))
             {
-                m_Scores[name] = score;
+                m_Scores[name] = new Points(score);
             }
             else
             {
-                m_Scores.Add(name, score);
+                m_Scores.Add(name, new Points(score));
+            }
+        }
+
+        public void AddResult(string name, Points points)
+        {
+            if (m_Scores.ContainsKey(name))
+            {
+                m_Scores[name] = points;
+            }
+            else
+            {
+                m_Scores.Add(name, points);
             }
         }
 
         internal Dictionary<string, double> GetResults()
         {
-            return m_Scores;
+            var oldResults = new Dictionary<string, double>();
+            foreach (var score in m_Scores)
+            {
+                oldResults.Add(score.Key, score.Value.GetValue());
+            }
+            return oldResults;
         }
     }
 }
