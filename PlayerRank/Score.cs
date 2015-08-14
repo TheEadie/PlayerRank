79fde45c315cab83d16afa1d0ccde45c195fa918
David Eadie <david.eadie@red-gate.com>
2015-08-14 13:47:08 +0100

    Green: score objects are equal

--- PlayerRank/Score.cs
+++ PlayerRank/Score.cs
@@ -8,5 +8,22 @@
         {
             m_Score = score;
         }
+
+        protected bool Equals(Score other)
+        {
+            return m_Score.Equals(other.m_Score);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj.GetType() == GetType() && Equals((Score) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return m_Score.GetHashCode();
+        }
     }
 }
