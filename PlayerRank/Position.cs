ac4a186256f5799df4201a03a9296bc66cb7417d
David Eadie <david.eadie@red-gate.com>
2015-08-27 09:34:31 +0100

    Green: Can order positions

--- PlayerRank/Position.cs
+++ PlayerRank/Position.cs
@@ -1,6 +1,8 @@
-﻿namespace PlayerRank
+﻿using System;
+
+namespace PlayerRank
 {
-    public class Position
+    public class Position : IComparable
     {
         private readonly int m_Position;
         
@@ -24,6 +26,32 @@
         public override int GetHashCode()
         {
             return m_Position.GetHashCode();
+        }
+
+        public int CompareTo(object obj)
+        {
+            var other = obj as Position;
+
+            if (other == null)
+                throw new ArgumentException("Can not compare Points to other type");
+
+            if (other > this)
+                return -1;
+            if (other < this)
+                return 1;
+
+            return 0;
+
+        }
+
+        public static bool operator >(Position pointsA, Position pointsB)
+        {
+            return (pointsA.m_Position > pointsB.m_Position);
+        }
+
+        public static bool operator <(Position pointsA, Position pointsB)
+        {
+            return (pointsA.m_Position < pointsB.m_Position);
         }
     }
 }
