fc5e2f8e40c3a4287bfb180adb6aff8e4e769dfb
David Eadie <david.eadie@red-gate.com>
2015-10-02 15:36:32 +0100

    Green: Can get result stats between 2 games

--- PlayerRank/Stats/ResultChangeStats.cs
+++ PlayerRank/Stats/ResultChangeStats.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PlayerRank.Stats
 {
@@ -7,7 +8,30 @@ namespace PlayerRank.Stats
     {
         public static IEnumerable<ResultsChange> GetResultChangesBewteenGames(IList<History> history, int gameA, int gameB)
         {
-            throw new NotImplementedException();
+            var changes = new List<ResultsChange>();
+
+            var players = history[gameA - 1].Game.GetResults().Select(x => x.Name);
+
+            var leaderboardA = history[gameA - 1].Leaderboard;
+            var leaderboardB = history[gameB - 1].Leaderboard;
+
+            foreach (var player in players)
+            {
+                var resultA = leaderboardA.Single(x => x.Name == player);
+                var resultB = leaderboardB.SingleOrDefault(x => x.Name == player);
+
+                if (resultB == null)
+                {
+                    resultB = new PlayerScore(player) {Points = new Points(0), Position = new Position(0)};
+                }
+
+                var positionChange = (resultA.Position.GetValue() - resultB.Position.GetValue());
+                var pointsChange = (resultA.Points - resultB.Points).GetValue();
+
+                changes.Add(new ResultsChange(player, positionChange, pointsChange));
+            }
+
+            return changes;
         }
     }
 }
