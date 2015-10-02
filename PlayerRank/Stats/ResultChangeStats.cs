302293ed9bccb598923485a59780904c1ad22a39
David Eadie <david.eadie@red-gate.com>
2015-10-02 15:23:49 +0100

    Red: Can get change in results stats from history

---
+++ PlayerRank/Stats/ResultChangeStats.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace PlayerRank.Stats
+{
+    public static class ResultChangeStats
+    {
+        public static IEnumerable<ResultsChange> GetResultChangesBewteenGames(IList<History> history, int gameA, int gameB)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PlayerRank/Stats/ResultsChange.cs
@@ -0,0 +1,16 @@
+﻿namespace PlayerRank.Stats
+{
+    public class ResultsChange
+    {
+        public string Name { get; private set; }
+        public int PositionChange { get; private set; }
+        public double PointsChange { get; private set; }
+
+        public ResultsChange(string name, int positionChange, double pointsChange)
+        {
+            PositionChange = positionChange;
+            PointsChange = pointsChange;
+            Name = name;
+        }
+    }
+}
