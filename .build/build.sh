e0077c5321920eae155def8d75d3dd06fa009137
David Eadie <david@eadie.net>
2020-04-09 22:29:23 +0100

    Add release script

--- .build/build.sh
+++ .build/build.sh
@@ -1,13 +1,23 @@
+#!/bin/bash
+
 # Install pre-reqs
 dotnet tool install -g GitVersion.Tool
+
+# Clear artifacts folder
+rm .artifacts/ -rf
+mkdir .artifacts
 
 # Calculate the version
 GitVersionOutput=dotnet-gitversion
 Version=$($GitVersionOutput | jq -r '.MajorMinorPatch')
 NuGetVersion=$($GitVersionOutput | jq -r '.NuGetVersion')
 
+echo "Writing version to file"
+touch .artifacts/version.json
+$GitVersionOutput > .artifacts/version.json
+
 # Build
-echo "Building version:" $VERSION
+echo "Building version:" $Version
 dotnet build -c Release /p:Version=$Version
 
 # Test
@@ -15,6 +25,5 @@ echo "Running tests"
 dotnet test --logger "trx;LogFileName=PlayerRank.Tests.trx"
 
 # Create NuGet Package
-echo "Creating NuGet Package"
-rm .artifacts/ -rf
+echo "Creating NuGet Package:" $NuGetVersion
 dotnet pack -c Release -o ".artifacts/" /p:Version=$Version /p:PackageVersion=$NuGetVersion
