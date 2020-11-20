83ec3a0df9d521cc61ec20ba443ee8056c9f378d
David Eadie <david@eadie.net>
2020-11-20 14:34:34 +0000

    Use headings in build script

--- .build/build.sh
+++ .build/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source `dirname "$0"`/functions.sh
 
 # Dependencies
 DockerImage_GitVersion="gittools/gitversion:5.3.4-linux-alpine.3.10-x64-netcoreapp3.1"
@@ -8,23 +9,22 @@ rm .artifacts/ -rf
 mkdir .artifacts
 
 # Calculate the version
-echo "Calculating next version"
+WriteHeading "Calculating next version"
 GitVersionOutput="echo $(docker run --rm -v "$(pwd):/repo" $DockerImage_GitVersion /repo)"
 Version=$($GitVersionOutput | jq -r '.MajorMinorPatch')
 NuGetVersion=$($GitVersionOutput | jq -r '.NuGetVersion')
 
-echo "Writing version to file"
 touch .artifacts/version.json
 $GitVersionOutput > .artifacts/version.json
 
 # Build
-echo "Building version:" $Version
+WriteHeading "Building version: $Version"
 dotnet build -c Release /p:Version=$Version
 
 # Test
-echo "Running tests"
+WriteHeading "Running tests"
 dotnet test --logger "trx;LogFileName=PlayerRank.Tests.trx"
 
 # Create NuGet Package
-echo "Creating NuGet Package:" $NuGetVersion
+WriteHeading "Creating NuGet Package: $NuGetVersion"
 dotnet pack -c Release -o ".artifacts/" /p:Version=$Version /p:PackageVersion=$NuGetVersion
